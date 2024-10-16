05c11bd55b9f285dfd9b9480cadfb8cfa9a8b9a8
Ki Ma <kima@KinoMacBook-Air-2.local>
2024-10-16 20:54:16 +0900

    multiple-results.go

--- gotour/main.go
+++ gotour/main.go
@@ -2,10 +2,11 @@ package main
 
 import "fmt"
 
-func add(x, y int) int {
-	return x + y
+func add(x, y string) (string, string) {
+	return x, y
 }
 
 func main() {
-	fmt.Println(add(42, 13))
+	a, b := add("hello", "world")
+	fmt.Println(a, b)
 }
