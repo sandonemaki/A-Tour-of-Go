c94d2898f511042764417b27f3bce2d360a76062
Ki Ma <kima@KinoMacBook-Air-2.local>
2024-10-18 21:00:28 +0900

    for

--- gotour/main.go
+++ gotour/main.go
@@ -2,16 +2,10 @@ package main
 
 import "fmt"
 
-const (
-	Big   = 1 << 100
-	Small = Big >> 99
-)
-
-func needInt(x int) int           { return x*10 + 1 }
-func needFloat(x float64) float64 { return x * 0.1 }
-
 func main() {
-	fmt.Println(needInt(Small))
-	fmt.Println(needFloat(Small))
-	fmt.Println(needFloat(Big))
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
 }
