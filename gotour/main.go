cf29b2f811d01e09b4aff14787187ccc869d8ea9
Ki Ma <kima@KinoMacBook-Air-2.local>
2024-10-23 16:48:28 +0900

    Switch evaluation orde

--- gotour/main.go
+++ gotour/main.go
@@ -2,21 +2,24 @@ package main
 
 import (
 	"fmt"
-	"runtime"
+	"time"
 )
 
 func main() {
-	fmt.Print("Go runs on")
-	// osは評価される値
-	//runtime.GOOS は、Goの組み込みパッケージである runtime パッケージの変数が格納。
-	switch os := runtime.GOOS; os {
-	case "darwin":
-		fmt.Println("OS X.")
-	case "linux":
-		fmt.Println("Linux.")
+	fmt.Print("When's Saturday?")
+	// 現在の曜日を time.Weekday 型で返す。
+	// 曜日に対応する整数値（0: 日曜日, 1: 月曜日, …, 6: 土曜日）。
+	today := time.Now().Weekday()
+	// time.Weekday 型の定数で、6 という整数値に対応
+	// 6（土曜日）に対する switch 文が実行
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Today.")
+	case today + 1:
+		fmt.Println("Tomorrow.")
+	case today + 2:
+		fmt.Println("In two days.")
 	default:
-		// freebsd, openbsd,
-		// plan9, windows...
-		fmt.Printf("%s.\n", os)
+		fmt.Println("Too far away.")
 	}
 }
