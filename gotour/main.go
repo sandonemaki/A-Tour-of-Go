5f4a6c9b786fbdc686dfd65c6243c9b83883079d
Ki Ma <kima@KinoMacBook-Air-2.local>
2024-10-23 17:13:18 +0900

    stacking defers

--- gotour/main.go
+++ gotour/main.go
@@ -5,13 +5,28 @@ import (
 )
 
 func main() {
-	defer fmt.Println("World")
+	defer fmt.Println("counting")
 
-	fmt.Println("hello")
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+	fmt.Println("done")
 }
 
-// hello
-// World
+// done
+// 9
+// 8
+// 7
+// 6
+// 5
+// 4
+// 3
+// 2
+// 1
+// 0
 
-// defer へ渡した関数の引数は、すぐに評価されますが、
-// その関数自体は呼び出し元の関数がreturnするまで実行されません。
+// defer へ渡した関数が複数ある場合、
+// その呼び出しはスタック( stack )されます。
+
+// 呼び出し元の関数がreturnするとき、
+// defer へ渡した関数は LIFO(last-in-first-out) の順番で実行されます。
