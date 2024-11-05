d5fcccacc3915402f22cd37daaeb43280da31737
Ki Ma <kima@KinoMacBook-Air-2.local>
2024-11-05 20:12:59 +0900

    pointers to structs

--- gotour/main.go
+++ gotour/main.go
@@ -9,7 +9,12 @@ type Vertex struct {
 }
 
 func main() {
-	v := Vertex{1, 2}
-	v.X = 4
-	fmt.Println(v.X)
+	v := Vertex{1, 2} // Vertex構造体の初期化
+	p := &v           // vのポインタを取得
+	// 1e9 は Go では 1000000000 として扱われます
+	p.X = 1e9      // ポインタを通じてXフィールドを変更
+	fmt.Println(v) // vの内容を出力
 }
+
+// X フィールドが 1000000000 に、Y フィールドが 2 のままである
+// {1000000000 2}
