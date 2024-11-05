38d0569e66074ea7f3199bf3f249cbbcb71f2dc7
Ki Ma <kima@KinoMacBook-Air-2.local>
2024-11-05 22:27:34 +0900

    Slices are like references to arrays

--- gotour/main.go
+++ gotour/main.go
@@ -5,9 +5,27 @@ import (
 )
 
 func main() {
-	primes := [6]int{2, 3, 5, 7, 11, 13}
-	var s []int = primes[1:4]
-	fmt.Println(s)
+	names := [4]string{
+		"John",
+		"Paul",
+		"George",
+		"Ringo",
+	}
+	fmt.Println(names)
+
+	a := names[0:2]
+	b := names[1:3]
+	fmt.Println(a, b)
+
+	// a と b の両方が同じ配列 names の異なる部分を指していることです。
+	// さらに、a と b は names の同じメモリ領域（インデックス 1 の要素 "Paul"）を指しています。
+	// このため、スライス b でインデックス 0 の要素を変更すると、スライス a にも影響が出ます。
+	b[0] = "XXX"
+	fmt.Println(a, b)
+	fmt.Println(names)
 }
 
-// [3 5 7]
+// [John Paul George Ringo]
+// [John Paul] [Paul George]
+// [John XXX] [XXX George]
+// [John XXX George Ringo]
