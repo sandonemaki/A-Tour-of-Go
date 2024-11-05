9c5c414052efd8273d4f10a3020322a4438e14ed
Ki Ma <kima@KinoMacBook-Air-2.local>
2024-11-06 01:06:17 +0900

    Creating a slice with make

--- gotour/main.go
+++ gotour/main.go
@@ -2,13 +2,23 @@ package main
 
 import "fmt"
 
+// https://scrapbox.io/maki-Py/Go%E3%81%AE%E3%82%B9%E3%83%A9%E3%82%A4%E3%82%B9%E3%81%AE%E5%AE%B9%E9%87%8F%E3%81%AE%E5%AE%9A%E7%BE%A9
+
 func main() {
-	var s []int
-	fmt.Println(s, len(s), cap(s))
-	if s == nil {
-		fmt.Println("nil!")
-	}
+	a := make([]int, 5)
+	printSlice("a", a)
+
+	b := make([]int, 0, 5)
+	printSlice("b", b)
+
+	c := b[:2]
+	printSlice("c", c)
+
+	d := c[2:5]
+	printSlice("d", d)
 }
 
-// [] 0 0
-// nil!
+func printSlice(s string, x []int) {
+	fmt.Printf("%s len=%d cap=%d %v\n",
+		s, len(x), cap(x), x)
+}
