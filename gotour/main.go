50475c7236e5e573977045d2a2b0711b73fafd7d
Ki Ma <kima@KinoMacBook-Air-2.local>
2024-11-07 21:08:55 +0900

    Function values

--- gotour/main.go
+++ gotour/main.go
@@ -1,21 +1,31 @@
 package main
 
 import (
-	"github.com/atotto/go-tour-jp/wc"
+	"fmt"
+	"math"
 )
 
-// WordCount は与えられた文字列内の単語の出現回数をカウントし、map で返します。
-func WordCount(s string) map[string]int {
-	// 仮の実装として map[string]int{"x": 1} を返していますが、
-	// 実際には入力された文字列 s を単語に分割し、
-	// それぞれの単語の出現回数をカウントして
-	// map[string]int を返すように実装する必要があります。
-	return map[string]int{"x": 1}
+// compute 関数は、引数として２つの float64 型の値をとり、float64 型の結果を返す関数を受け取ります。
+func compute(fn func(float64, float64) float64) float64 {
+	// 引数として渡された fn を 3 と 4 という引数で実行し結果を返します。
+	return fn(3, 4)
 }
 
 func main() {
-	// 関数のテストを実行
-	// wc パッケージには、文字列を単語ごとに分割して
-	// その出現回数を数える関数をテストするための便利な関数 Test が含まれています。
-	wc.Test(WordCount)
+	// hypot は匿名関数で2つの float64 型の引数 x と y を受け取り、その平方根を返します。
+	// これは三角形の斜辺（hypotenuse）を求めるための計算
+	hypot := func(x, y float64) float64 {
+		return math.Sqrt(x*x + y*y)
+	}
+	// hypot 関数を実行
+	fmt.Println(hypot(5, 12))
+
+	// compute 関数に hypot を渡し、3 と 4 を引数として計算した結果を出力する
+	fmt.Println(compute(hypot))
+	// compute 関数に math.Pow を渡し、3^4 を計算して出力
+	fmt.Println(compute(math.Pow))
 }
+
+// 13
+// 5
+// 81
