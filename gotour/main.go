5ab7e44091be5b5f84b8acc1f79f1c4257b947b8
Ki Ma <kima@KinoMacBook-Air-2.local>
2024-11-07 11:49:36 +0900

    Mutating Maps

--- gotour/main.go
+++ gotour/main.go
@@ -4,20 +4,23 @@ import (
 	"fmt"
 )
 
-// Vertex は緯度 (Lat) と経度 (Long) の2つの float64 型のフィールドを持つ構造体
-type Vertex struct {
-	Lat, Long float64
-}
-
-// map[string]Vertex 型のマップ m を作成
-// キーとして string 型、値として Vertex 型を持つマップ
-// mapに渡すトップレベルの型が単純な型名である場合は、
-// リテラルの要素から推定できますので、その型名を省略することができます。
-var m = map[string]Vertex{
-	"Bell Labs": {40.68433, -74.39967},
-	"Google":    {37.42202, -122.08408},
-}
-
 func main() {
-	fmt.Println(m)
+	m := make(map[string]int)
+
+	m["Answer"] = 42
+	fmt.Println("The value:", m["Answer"])
+
+	m["Answer"] = 48
+	fmt.Println("The value:", m["Answer"])
+
+	delete(m, "Answer")
+	fmt.Println("The value:", m["Answer"])
+
+	v, ok := m["Answer"]
+	fmt.Println("The value:", v, "Present?", ok)
 }
+
+// The value: 42
+// The value: 48
+// The value: 0
+// The value: 0 Present? false
