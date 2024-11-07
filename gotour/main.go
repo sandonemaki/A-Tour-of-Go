e700418c5729f18f01a22790cb760bb62eb52153
Ki Ma <kima@KinoMacBook-Air-2.local>
2024-11-07 13:30:34 +0900

    Exercise: Maps

--- gotour/main.go
+++ gotour/main.go
@@ -1,26 +1,21 @@
 package main
 
 import (
-	"fmt"
+	"github.com/atotto/go-tour-jp/wc"
 )
 
-func main() {
-	m := make(map[string]int)
-
-	m["Answer"] = 42
-	fmt.Println("The value:", m["Answer"])
-
-	m["Answer"] = 48
-	fmt.Println("The value:", m["Answer"])
-
-	delete(m, "Answer")
-	fmt.Println("The value:", m["Answer"])
-
-	v, ok := m["Answer"]
-	fmt.Println("The value:", v, "Present?", ok)
+// WordCount は与えられた文字列内の単語の出現回数をカウントし、map で返します。
+func WordCount(s string) map[string]int {
+	// 仮の実装として map[string]int{"x": 1} を返していますが、
+	// 実際には入力された文字列 s を単語に分割し、
+	// それぞれの単語の出現回数をカウントして
+	// map[string]int を返すように実装する必要があります。
+	return map[string]int{"x": 1}
 }
 
-// The value: 42
-// The value: 48
-// The value: 0
-// The value: 0 Present? false
+func main() {
+	// 関数のテストを実行
+	// wc パッケージには、文字列を単語ごとに分割して
+	// その出現回数を数える関数をテストするための便利な関数 Test が含まれています。
+	wc.Test(WordCount)
+}
