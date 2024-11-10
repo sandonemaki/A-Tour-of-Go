eb20094ac42646344c649fd91864fb5f77c853e8
Ki Ma <kima@KinoMacBook-Air-2.local>
2024-11-11 07:43:30 +0900

    Exercise: Fibonacci closure

--- gotour/main.go
+++ gotour/main.go
@@ -4,27 +4,39 @@ import (
 	"fmt"
 )
 
-// adder 関数が「func(int) int 型の関数」を返す
-func adder() func(int) int {
-	sum := 0
-	// adder は func(x int) int { sum += x; return sum } という関数を返す
-	return func(x int) int {
-		// x を受け取って sum に加え、更新された sum の値を返す
-		sum += x
-		return sum
+// fibonacci 関数が「func(int) int 型の関数」を返す
+func fibonacci() func(int) int {
+	// 初期化
+	a, b := 0, 1
+	return func(n int) int {
+		if n == 0 {
+			return a
+		} else if n == 1 {
+			return b
+		} else {
+			// フィボナッチ数列の計算
+			a, b = b, a+b
+			return b
+		}
 	}
 }
 
 func main() {
-	// pos や neg という変数には、adder によって返されたクロージャ（無名関数）が格納。
-	// この時点で adder は終了。
-	pos, neg := adder(), adder()
+	// fibonacci() 関数からクロージャを取得し、変数 f に保存する。
+	// これにより、f は状態を保持したまま呼び出すたびに次のフィボナッチ数を計算する関数となる。
+	f := fibonacci()
 	for i := 0; i < 10; i++ {
-		fmt.Println(
-			// pos や neg に格納されたクロージャが実行されるが、adder 関数は再度実行されない。
-			//これらのクロージャは、sum の状態を保持し続ける性質を持っている
-			pos(i),    // pos に格納されたクロージャが i を受け取り pos の sum を更新し、新しい値を返す。
-			neg(-2*i), // neg に格納されたクロージャが -2*i を受け取り neg の sum を更新して返す。
-		)
+		fmt.Println(f(i))
 	}
 }
+
+// 0
+// 1
+// 1
+// 2
+// 3
+// 5
+// 8
+// 13
+// 21
+// 34
