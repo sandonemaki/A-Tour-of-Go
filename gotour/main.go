feaa8c432eaf398aac3a5f8edf6a80d26341c2ca
Ki Ma <kima@KinoMacBook-Air-2.local>
2024-11-12 23:05:24 +0900

    Pointers and functions

--- gotour/main.go
+++ gotour/main.go
@@ -10,17 +10,13 @@ type Vertex struct {
 	X, Y float64
 }
 
-// Vertex 型の変数 v に対して Abs というメソッドを定義
-// Abs メソッドは変数レシーバ
-// これはレシーバのコピーを渡します。メソッド内での操作は元の変数に影響を与えません。
-func (v Vertex) Abs() float64 {
+// Abs 関数は Vertex型の変数 v に対して実行される
+func Abs(v Vertex) float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
-// Vertex 型のポインタ v に対して Scale というメソッドを定義
-// Scale メソッドはポインタレシーバ
-// 元の変数へのポインタを渡します。メソッド内での操作は元の変数自体を変更できます。
-func (v *Vertex) Scale(f float64) {
+// Scale 関数はポインタVertex型の変数 v と変数 f に対して実行される。
+func Scale(v *Vertex, f float64) {
 	v.X = v.X * f
 	v.Y = v.Y * f
 }
@@ -28,21 +24,18 @@ func (v *Vertex) Scale(f float64) {
 func main() {
 	// Vertex 型の変数 v を初期化
 	v := Vertex{3, 4}
-	// v の Scale メソッドを呼び出し
-	// ポインタレシーバであるため、v 自体が変更される
-	v.Scale(10)
-	// v の Abs メソッドを呼び出し
-	// 元の v の X と Y が変更された後の値を出力
-	fmt.Println(v.Abs())
+	// ポインタ v　と 10 を引数に Scale 関数を呼び出し
+	Scale(&v, 10)
+	// 変数 v を引数に Abs 関数を呼び出し
+	fmt.Println(Abs(v))
 }
 
-// Abs メソッドは 変数レシーバ なので、
-// 元の変数 v のコピーが渡されてメソッド内の変更は元の v に影響を与えない。
-// 一方、Scale メソッドは ポインタレシーバ なので、
-// 元の変数 v へのポインタが渡され、メソッド内での操作が元の変数 v 自体に反映される。
-
-// 要点
-// ・変数レシーバ: コピーが渡され、元の値は変更されない。
-// ・ポインタレシーバ: ポインタが渡され、元の値が変更される。
-
 // 50
+
+// もし Scale 関数の引数を *Vertex から Vertex に変更すると、
+// 関数内で v のコピーが渡されるため、Scale 関数内の変更は元の v に反映されません。
+
+// ポインタ引数 (*Vertex):
+// ・Scale 内の操作が元の v に反映される。
+// 値引数 (Vertex):
+// ・Scale 内の操作はコピーに対して行われ、元の v には影響しない。
