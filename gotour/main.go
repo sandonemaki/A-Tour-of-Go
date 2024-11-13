bbcf084f339e969914c8cb4ed0b64c2e5ff966ce
Ki Ma <kima@KinoMacBook-Air-2.local>
2024-11-14 01:26:52 +0900

    The empty interface

--- gotour/main.go
+++ gotour/main.go
@@ -4,23 +4,29 @@ import (
 	"fmt"
 )
 
-// インターフェース I は M() メソッドを持つインターフェース
-type I interface {
-	M()
-}
-
 func main() {
-	// インターフェース I は M() メソッドを持つ
-	var i I
+	// interface i は 0 個のメソッドを持つインターフェース
+	// interface{} は空のインターフェースで、Go では 任意の型 を保持できる特別なインターフェース。
+	var i interface{}
+	describe(i)
 
-	describe(i) // 出力：(<nil>, <nil>)
-	// M() メソッドが i を nil として受け取る。
-	// nil pointer dereference というランタイムエラーが発生
-	// panic: runtime error: invalid memory address or nil pointer dereference
-	i.M() // nil dereference in dynamic method call
+	// 空のインターフェースは、任意の型の値を保持できる。
+	// i は int 型の値 42 を保持
+	i = 42
+	describe(i)
+
+	// i は string 型の値 "hello" を保持
+	i = "hello"
+	describe(i)
 }
 
-// describe 関数はインターフェース I 型の変数 i を引数にとり表示
-func describe(i I) {
+// describe 関数の引数に空のインターフェイス i が渡され値を表示
+// 空のインターフェースは、未知の型の値を扱うコードで使用される。
+// interface{} 型の任意の数の引数を受け取る。
+func describe(i interface{}) {
 	fmt.Printf("(%v, %T)\n", i, i)
 }
+
+// (<nil>, <nil>)
+// (42, int)
+// (hello, string)
