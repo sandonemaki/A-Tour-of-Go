1562bdc0309ab26f346c9bbd43b76cf36b49c2e2
Ki Ma <kima@KinoMacBook-Air-2.local>
2024-11-15 02:09:30 +0900

    Exercise: Loops and Functions

--- gotour/main.go
+++ gotour/main.go
@@ -2,44 +2,34 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
-type MyError struct {
-	When time.Time
-	What string
+type ErrNegativeSqrt float64
+
+// ErrNegativeSqrt 型の Error メソッド
+func (e ErrNegativeSqrt) Error() string {
+	// fmt.Sprint(e) を直接呼び出すと e の Error() メソッドが再帰的に呼び出されるため無限ループになる。
+	// これを避けるために float64(e) に型変換し単なる数値として出力する。
+	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
 }
 
-// MyError ポイントレシーバを持つ Error メソッドが string 型を返す
-// このメソッドは error インターフェースを実装しています。
-// error インターフェースは Error() string というメソッドシグネチャを持ち、
-// エラーメッセージを文字列として返すことが求められます。
-func (e *MyError) Error() string {
-	// MyError 型の When フィールドを文字列に変換して返す
-	// e.When は MyError 型の When フィールドで、型は time.Time
-	// e.When.String() は time.Time 型の String() メソッドを呼び出し、When を文字列に変換
-	// "2024-11-15 15:04:05.123456" のような形式で時間が表現
-	return e.When.String() + ": " + e.What
-}
-
-// run 関数は error 型を返す
-// 戻り値として MyError 型のインスタンスのポインタを返す。
-func run() error {
-	return &MyError{
-		time.Now(),
-		"it didn't work",
+// Sqrt 関数の修正
+func Sqrt(x float64) (float64, error) {
+	// Sqrt 関数に正の数と負の数を渡し、エラーメッセージを確認します。
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
 	}
+	// 簡易的なニュートン法による近似
+	z := x
+	for i := 0; i < 10; i++ {
+		z -= (z*z - x) / (2 * z)
+	}
+	return z, nil
 }
 
 func main() {
-	if err := run(); err != nil {
-		fmt.Println(err)
-	}
+	// 正の数のテスト
+	fmt.Println(Sqrt(2))
+	// 負の数のテスト
+	fmt.Println(Sqrt(-2))
 }
-
-// 2024-11-15 01:40:24.886132 +0900 JST m=+0.000101126: it didn't work
-
-// run() 関数を呼び出すと、MyError のインスタンスが作成され、そのポインタが error 型として返される。
-// main() 関数でこの run() の戻り値を err 変数に代入し、エラーチェックを行う。
-// エラーが存在する場合、fmt.Println(err) などで出力すると、
-// MyError 型の Error() メソッドが自動的に呼ばれ、カスタムメッセージが表示される。
