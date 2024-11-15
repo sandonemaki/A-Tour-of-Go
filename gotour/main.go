69d57243c2db96db52f367ea3fbe5c4b2e418caa
Ki Ma <kima@KinoMacBook-Air-2.local>
2024-11-16 01:05:06 +0900

    Buffered Channels2

--- gotour/main.go
+++ gotour/main.go
@@ -3,28 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	ch := make(chan int, 3) // バッファサイズ3のチャネル
-
-	// バッファにデータを送信
+	ch := make(chan int, 2)
 	ch <- 1
 	ch <- 2
-	ch <- 3
-
-	fmt.Println("3つのデータをバッファに送信しました")
-
-	// バッファがいっぱいなので、次の送信でブロックされる
-	go func() {
-		ch <- 4
-		fmt.Println("4をチャネルに送信しました")
-	}()
-
-	fmt.Println("受信を開始します")
-
-	// チャネルからデータを受信
 	fmt.Println(<-ch)
-	fmt.Println(<-ch)
-	fmt.Println(<-ch)
-
-	// これで4つ目のデータが送信され、ブロックが解除される
 	fmt.Println(<-ch)
 }
