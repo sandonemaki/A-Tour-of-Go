f3a86b3e154d23c1b36b5744cefab6c235216684
Ki Ma <kima@KinoMacBook-Air-2.local>
2024-11-15 20:05:43 +0900

    Exercise: Images

--- gotour/main.go
+++ gotour/main.go
@@ -18,6 +18,7 @@ func (img Image) Bounds() image.Rectangle {
 }
 
 // ColorModel メソッドの実装
+// 画像が表示されるときに呼び出される
 func (img Image) ColorModel() color.Model {
 	return color.RGBAModel
 }
@@ -25,6 +26,9 @@ func (img Image) ColorModel() color.Model {
 // At メソッドの実装
 func (img Image) At(x, y int) color.Color {
 	// ピクセルの色の値を計算
+	// % 256 にすることで、v は 0〜255 の範囲に収まる
+	// 青を255（最大値）に設定しているため、青っぽい色が基調になる
+	// 最後の 255 はアルファ値（不透明度）
 	v := uint8((x + y) % 256)
 	return color.RGBA{v, v, 255, 255}
 }
@@ -32,5 +36,6 @@ func (img Image) At(x, y int) color.Color {
 func main() {
 	// Image インスタンスを生成し、画像を表示
 	m := Image{width: 256, height: 256}
+	// 画像を生成して表示
 	pic.ShowImage(m)
 }
